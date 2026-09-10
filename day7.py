f080efcf56fcd0beac97ee935809f3745fc07e28
Eva <evamchowdhuryis@gmail.com>
2026-09-10 16:34:05 +0000

    day7incomplete

---
+++ day7.py
@@ -0,0 +1,16 @@
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+print(len(it_companies))
+it_companies.add("Twitter")
+it_companies.update(["Dell", "LG", "Aseus"])
+it_companies.remove("Facebook")
+#remove will return ERROR if item doesnt exist, discard will not
+
+AB = A.union(B)
+intercept = A.intersection(B)
+print(intercept)
+print(A.issubset(B))
+print(A.isdisjoint(B))
